5693e284fecdae5598415aa65b53aacfc032a808
Michael.L <126206742+MLiondis@users.noreply.github.com>
2025-02-08 04:17:18 +0000

    finnished conditionals prob 3

---
+++ 1-Conditionals/ext/ext.py
@@ -0,0 +1,16 @@
+type = input("Type? ")
+
+if ".jpg" in type:
+    print("image/jpeg")
+elif ".jpeg" in type:
+    print("image/jpeg")
+elif ".gif" in type:
+    print("image/gif")
+elif ".png" in type:
+    print("image/png")
+elif ".pdf" in type:
+    print("applicaition/pdf")
+elif ".txt" in type:
+    print("text/plain")
+elif ".zip" in type:
+    print("application/zip")
