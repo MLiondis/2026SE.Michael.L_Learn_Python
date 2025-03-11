527a3b231ec56bfd36b2d2f390b42a4b36eb14d9
Michael.L <126206742+MLiondis@users.noreply.github.com>
2025-03-11 22:35:56 +0000

    When you peek into bros code and see 18 generations of if statements:

---
+++ permu.py
@@ -0,0 +1,22 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+
+for i in lst:
+    for j in lst:
+        for k in lst:
+            for l in lst:
+                for m in lst:
+                    for n in lst:
+                        for o in lst:
+                            for p in lst:
+                                for q in lst:
+                                    for r in lst:
+                                        for s in lst:
+                                            for t in lst:
+                                                for u in lst:
+                                                    for v in lst:
+                                                        for w in lst:
+                                                            for x in lst:
+                                                                for y in lst:
+                                                                    for z in lst:
+                                                                        if i != j and j != k and i != k and i != l and i != m and i != n and i != o and i != p and i != q and i != r and i != s and i != t and i != u and i != v and i != w and i != x and i != y and i != z and j != l and j != m and j != n and j != o and j != p and j != q and j != r and j != s and j != t and j != u and j != v and j != w and j != x and j != y and j != z and k != l and k != m and k != n and k != o and k != p and k != q and k != r and k != s and k != t and k != u and k != v and k != w and k != x and k != y and k != z and l != m and l != n and l != o and l != p and l != q and l != r and l != s and l != t and l != u and l != v and l != w and l != x and l != y and l != z and m != n and m != o and m != p and m != q and m != r and m != s and m != t and m != u and m != v and m != w and m != x and m != y and m != z and n != o and n != p and n != q and n != r and n != s and n != t and n != u and n != v and n != w and n != x and n != y and n != z and o != p and o != q and o != r and o != s and o != t and o != u and o != v and o != w and o != x and o != y and o != z and p != q and p != r and p != s and p != t and p != u and p != v and p != w and p != x and p != y and p != z and q != r and q != s and q != t and q != u and q != v and q != w and q != x and q != y and q != z and r != s and r != t and r != u and r != v and r != w and r != x and r != y and r != z and s != t and s != u and s != v and s != w and s != x and s != y and s != z and t != u and t != v and t != w and t != x and t != y and t != z and u != v and u != w and u != x and u != y and u != z and v != w and v != x and v != y and v != z and w != x and w != y and w != z and x != y and x != z and y != z:
+                                                                            print(i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z)
