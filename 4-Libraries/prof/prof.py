8a991ed3b477f4d61d337cc6e44a05a443b3e341
Michael.L <126206742+MLiondis@users.noreply.github.com>
2025-03-23 09:20:23 +0000

    maybe read the fucking scrpit instead of spending 30 mins trying to figure out something you dont need. finsished prof.py. spacemanout

---
+++ 4-Libraries/prof/prof.py
@@ -0,0 +1,106 @@
+import random
+
+
+#def main():
+
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level 1, 2, or 3? "))
+            if 1 <= level <= 3:
+                print("Level is", level)
+                generate_integer(level)
+                break
+        except ValueError:
+            continue
+
+
+def generate_integer(level):
+    score = 0
+    while True:
+        if level == 1:
+            for i in range(10):
+                x = random.randint(1, 9)
+                y = random.randint(1, 9) 
+                z = x + y
+                user_answer = int(input(f"{x} + {y} = "))
+                if user_answer == z:
+                    score += 1
+                    continue
+                elif user_answer != z:
+                    print("EEE")
+                    user_answer2 = int(input(f"{x} + {y} = "))
+                    if user_answer2 == z:
+                        score += 1
+                        continue
+                    elif user_answer2 != z:
+                        print("EEE")
+                        user_answer3 = int(input(f"{x} + {y} = "))
+                        if user_answer3 == z:
+                            score += 1
+                        elif user_answer3 != z:
+                            print(f"{x} + {y} = {z}")
+                            continue
+            print("Your score is", score,"/10")
+            break
+        elif level == 2:
+            for i in range(10):
+                x = random.randint(10, 99)
+                y = random.randint(10, 99)
+                z = x + y
+                user_answer = int(input(f"{x} + {y} = "))
+                if user_answer == z:
+                    score += 1
+                    continue
+                elif user_answer != z:
+                    print("EEE")
+                    user_answer2 = int(input(f"{x} + {y} = "))
+                    if user_answer2 == z:
+                        score += 1
+                        continue
+                    elif user_answer2 != z:
+                        print("EEE")
+                        user_answer3 = int(input(f"{x} + {y} = "))
+                        if user_answer3 == z:
+                            score += 1
+                        elif user_answer3 != z:
+                            print(f"{x} + {y} = {z}")
+                            continue
+            print("Your score is", score,"/10")
+            break
+        elif level == 3:
+            for i in range(10):
+                x = random.randint(100, 999)
+                y = random.randint(100, 999)
+                z = x + y
+                user_answer = int(input(f"{x} + {y} = "))
+                if user_answer == z:
+                    score += 1
+                    continue
+                elif user_answer != z:
+                    print("EEE")
+                    user_answer2 = int(input(f"{x} + {y} = "))
+                    if user_answer2 == z:
+                        score += 1
+                        continue
+                    elif user_answer2 != z:
+                        print("EEE")
+                        user_answer3 = int(input(f"{x} + {y} = "))
+                        if user_answer3 == z:
+                            score += 1
+                        elif user_answer3 != z:
+                            print(f"{x} + {y} = {z}")
+                            continue
+            print("Your score is", score,"/10")
+            break
+
+
+
+
+
+
+
+#if __name__ == "__main__":
+#main()
+get_level()
