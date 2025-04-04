348c1f128357d0c32ea83d458c8c90e546b42c92
Michael.L <126206742+MLiondis@users.noreply.github.com>
2025-04-04 07:58:49 +0000

    6.2/pizza finished. bnous cowsay

---
+++ 6-FileIO/pizzza/pizza.py
@@ -0,0 +1,26 @@
+from tabulate import tabulate 
+import sys
+import csv
+import cowsay
+
+new_table = []
+
+if len(sys.argv) > 2:
+    print("Too many command-line arguments")
+    sys.exit(1)
+elif len(sys.argv) == 1:
+    print("Too few command-line arguments")
+    sys.exit(1)
+elif not sys.argv[1].endswith(".csv"):
+    print("Not a CSV file")
+    sys.exit(1)
+else:
+    try:
+        with open(sys.argv[1], "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                new_table.append(row)
+            cowsay.trex(tabulate(new_table, headers="keys", tablefmt="grid"))
+    except FileNotFoundError:
+        print("File does not exist")
+        sys.exit(1)
